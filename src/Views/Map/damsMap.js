912477ccee14a7572c990e19ae992510ec9d1187
Muhammad Kashif <34652365+mkashif113@users.noreply.github.com>
2020-02-10 13:07:10 +0500

    add dampin

--- src/Views/Map/damsMap.js
+++ src/Views/Map/damsMap.js
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 import "leaflet/dist/leaflet.css";
 import leaflet from "leaflet";
 import { Map, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
-import dam from "../../Assets/dam.png";
+import damPin from "../../Assets/dampin.png";
 import pakistanLocation from "../../Utilities/Constants/pakistan-geolocation.json";
 
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const pointerIcon = new leaflet.Icon({
-  iconUrl: dam,
+  iconUrl: damPin,
   iconAnchor: [5, 40],
   popupAnchor: [10, -44],
-  iconSize: [35, 65],
+  iconSize: [32, 52],
   shadowSize: [68, 95],
   shadowAnchor: [20, 92]
 });
@@ -118,7 +118,7 @@ class DamsMap extends Component {
           data={pakistanLocation}
           style={() => ({
             color: "#FFFFF0",
-            weight: 10,
+            weight: 3,
             fillColor: "green",
             fillOpacity: 0.6,
             opacity: 1
